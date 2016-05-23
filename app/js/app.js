b14cc590c9aa8e4b7fab10f160bc68f6cae29deb
marcos <marcos_alvarenga9@hotmail.com>
2016-05-23 12:44:11 -0300

    Inicio con el login. Acomodo estructura

--- app/js/app.js
+++ app/js/app.js
@@ -1,2 +1,22 @@
 'use strict';
-angular.module('laberintosApp', [ ])
+angular.module('laberintosApp', ["ui.router"])
+
+
+
+// angular.module('laberintosApp', ['ngRoute']);
+//ejemplo de routing 
+// .config(['$routeProvider', function ($routeProvider) {
+//     $routeProvider
+//         .when('/', {
+//             templateUrl: 'inicio.html',
+//             controller: 'InicioCtrl',
+ 
+//         })
+//         .when('/perfil', {
+//             templateUrl: 'perfil.html',
+//             controller: 'PerfilCtrl',
+
+//         .otherwise({
+//             redirectTo: '/'
+//         });
+// }]);
