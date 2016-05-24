812e21ad8bbe24330f7dd6e3457ea60ab0938340
marcos <marcos_alvarenga9@hotmail.com>
2016-05-24 13:45:11 -0300

    Inicio

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('laberintosApp', ["ui.router"])
+angular.module('laberintosApp', ['ui.router'])
 
 
 
--- app/js/directives.js
+++ app/js/directives.js
@@ -6,6 +6,6 @@ angular.module('laberintosApp', [])
 .directive('footerInfo', function() {
     return {
     	restrict : 'AE',
-    	templateUrl: 'partials/footer-info.html'
+    	templateUrl: 'app/partials/footer-info.html'
     }
 });
