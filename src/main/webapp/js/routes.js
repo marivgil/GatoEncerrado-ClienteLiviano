3826dcb9c9bb463398e97ad30ba008517d72b9cc
Maricruz <gil.maricruz@gmail.com>
2016-06-03 16:55:45 -0300

    inicio estructura nueva

---
+++ src/main/webapp/js/routes.js
@@ -0,0 +1,19 @@
+app.config(function ($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise("/");
+
+  $stateProvider
+
+    .state('pruebaUno', {
+      url: "/",
+      templateUrl: "app/partials/prueba_uno.html"
+      //controller: "AgregarTareaController as tareasCtrl"
+    })
+
+    // .state('prueba2', {
+    //   //url: "/tarea/:id",
+    //   templateUrl: "partials/prueba2.html",
+    //   //controller: "EditarTareaController as editarCtrl"
+    // })
+
+});
